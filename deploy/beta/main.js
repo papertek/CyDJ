36aedae41fcdb949070357ed608814e10e43216d
papertek <72106503+papertek@users.noreply.github.com>
2022-02-20 17:24:45 +0000

    Deploying to deploy from @ papertek/CyDJ@f239f6ddd8d03833835db965ab95ac2402009dbd 🚀

--- deploy/beta/main.js
+++ deploy/beta/main.js
@@ -4442,7 +4442,7 @@ var cydj = (function (exports) {
         if (AskAnswers_Array.length < 1) {
           AskAnswers_Array = ['yes', 'no'];
         }
-        const rnd = a = Math.round(Math.random() * (AskAnswers_Array.length - 1));
+        const rnd = Math.round(Math.random() * (AskAnswers_Array.length - 1));
         msg = AskAnswers_Array[rnd];
       } else if (msg.startsWith('!time')) {
         let h = new Date().getHours();
