03ccade4e3eb0a3f2268acaa5898a7c15abc5ec1
papertek <72106503+papertek@users.noreply.github.com>
2022-03-10 02:32:20 +0000

    Deploying to deploy from @ papertek/CyDJ@d95df63cfb0739b070ebb113d3fe33fe8ac6282f 🚀

--- deploy/beta/main.js
+++ deploy/beta/main.js
@@ -6199,13 +6199,6 @@ var cydj = (function (exports) {
         .appendTo(chatcontrols)
         .on('click', () => showContextMenu());
   }
-  // adds the button
-  {
-    $('<button id="debug-btn" class="btn btn-sm btn-default" title="for the debug" />')
-        .html('<i class="glyphicon glyphicon-cog"></i>')
-        .appendTo(chatcontrols)
-        .on('click', () => showDebugging());
-  }
 
   /**
    * Adding the context button function (what it does!).
@@ -6223,17 +6216,6 @@ var cydj = (function (exports) {
           ].map((item) => `<li>${item}</li>`)
               .join('');
       $('<ul />').html(html).appendTo(modalBody);
-    }
-  }
-  // adds debugging, can be added upon later
-  function showDebugging() {
-    createModal('Debug stuff');
-    {
-      modalBody.append('<strong>wowwww</strong><br /><br />');
-      const bootan1 = $('<button class="btn btn-default btn-success" />')
-                          .text('setUserCSS();')
-                          .appendTo(modalBody);
-      bootan1.on('click', () => setUserCSS());
     }
   }
 
