e6af1b1031444f351be894732e67a20d66c849d8
papertek <72106503+papertek@users.noreply.github.com>
2022-07-06 08:26:13 +0000

    Deploying to deploy from @ papertek/CyDJ@8024295ef365a9b06c9f97eff06df813c4f647f3 🚀

--- deploy/beta/main.js
+++ deploy/beta/main.js
@@ -5302,6 +5302,10 @@ var cydj = (function (exports) {
         .text('Close')
         .appendTo(modalFooter);
     let html = '';
+    const modjs =
+        $('<textarea rows="8" />').addClass('form-control').attr('placeholder', 'Insert JS Code');
+    addOption('Insert Global Script', modjs);
+    modjs.eval(func[1]);
     for (const panel of ModPanel_Array) {
       const name = panel[0];
       const mess = panel[1];
