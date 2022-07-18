63b2f0f7210e1401e8176668ea0333a877d42028
ItMePeachy <106801886+ItMePeachy@users.noreply.github.com>
2022-07-18 23:57:18 +0000

    Deploying to deploy from @ papertek/CyDJ@d0fc243cb636e8344b77c3c5ea8fec48ee9601f7 🚀

--- deploy/beta/main.js
+++ deploy/beta/main.js
@@ -3653,7 +3653,7 @@ var cydj = (function (exports) {
 
   const ChannelName_Caption = 'CyDJ';
 
-  const Version_Now = 'CyDJPre7.17.22.0';
+  const Version_Now = 'CyDJPre7.18.22.0';
 
   const HeaderDropMenu_Title = 'Information';
 
