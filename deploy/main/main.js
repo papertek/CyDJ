4ed0552eb2e40a160776af383f34faa99f602593
papertek <72106503+papertek@users.noreply.github.com>
2022-08-23 05:55:32 +0000

    Deploying to deploy from @ papertek/CyDJ@52bbbc8451560bd97f54b3e7bd93558d50b7673e 🚀

--- deploy/main/main.js
+++ deploy/main/main.js
@@ -3329,7 +3329,7 @@ var cydj = (function (exports) {
       Xqc.SUB_18_MONTHS_T3,
     ],
     'TommyLegamerQc': [
-      Xqc.SUB_12_MONTHS_T1,
+      Xqc.SUB_18_MONTHS_T1,
     ],
     'MDPog': [
       'https://cdn.discordapp.com/attachments/915656975696687124/916100799233425548/VIP.png',
@@ -3660,7 +3660,7 @@ var cydj = (function (exports) {
 
   const ChannelName_Caption = 'CyDJ';
 
-  const Version_Now = 'CyDJPre7.23.22.1';
+  const Version_Now = 'CyDJPre8.23.22.0';
 
   const HeaderDropMenu_Title = 'Information';
 
