80fb0add3fa8986120363df0b7c957ba9c852d2b
papertek <72106503+papertek@users.noreply.github.com>
2022-09-07 19:37:27 +0000

    Deploying to deploy from @ papertek/CyDJ@877978545297154effb5d8a430210e5f69a5aaa6 🚀

--- deploy/beta/main.js
+++ deploy/beta/main.js
@@ -5450,6 +5450,10 @@ var cydj = (function (exports) {
     $('<iframe id="previewFrame" width="558" height="314" frameborder="0" />')
         .attr('src', `https://www.youtube.com/embed/${a}?wmode=transparent&enablejsapi`)
         .appendTo(modalBody);
+    $('<button class ="btn btn-default" type="button" data-dismiss="modal"/>')
+        .text('Close')
+        .appendTo(modalFooter);
+    $('Preview Video').empty();
   }
 
   /**
