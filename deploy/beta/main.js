5abcc28c249895d236bf820b0bc4348daf0fc0b3
papertek <72106503+papertek@users.noreply.github.com>
2022-12-21 01:36:49 +0000

    Deploying to deploy from @ papertek/CyDJ@a38bc44d94b597fcf3513128fb5fe45037e4847f 🚀

--- deploy/beta/main.js
+++ deploy/beta/main.js
@@ -3531,8 +3531,6 @@ var cydj = (function (exports) {
   // Function taken from util.js.
   // This takes user commands and actually let's the user post them.
 
-  /*
-
   $('#chatline').on('keydown', (ev) => {
     if (ev.key === 'Enter') {
       if (CHATTHROTTLE) {
@@ -3552,72 +3550,6 @@ var cydj = (function (exports) {
         updateChatStats(_msg);
         $('#chatline').val('');
       }
-    }
-  });
-
-  */
-
-  let /** @type {string} */ unsentMsg = null;
-
-  $('#chatline').on('keydown', (ev) => {
-    if (ev.key === 'Enter') {
-      if (CHATTHROTTLE) {
-        return;
-      }
-      const _msg = $('#chatline').val();
-      let msg = $('#chatline').val();
-      if (msg.trim()) {
-        msg = prepareMessage(msg.trim());
-        const meta = {};
-        if (COMMAND) {
-          socket.emit('chatMsg', {msg: _msg});
-          msg = `➥ ${msg}`;
-          COMMAND = false;
-        }
-        if (USEROPTS.adminhat && CLIENT.rank >= 255) {
-          msg = `/a ${msg}`;
-        } else if (USEROPTS.modhat && CLIENT.rank >= Rank.Moderator) {
-          meta.modflair = CLIENT.rank;
-        }
-        if (CLIENT.rank >= 2 && msg.startsWith('/m ')) {
-          meta.modflair = CLIENT.rank;
-          msg = msg.substring(3);
-        }
-        if (msg.startsWith('/say')) {
-          meta.addClass = 'shout';
-          meta.forceShowName = true;
-          meta.addClassToNameAndTimestamp = true;
-        }
-        socket.emit('chatMsg', {msg: msg, meta: meta});
-        updateChatStats(_msg);
-        CHATHIST.push($('#chatline').val());
-        CHATHISTIDX = CHATHIST.length;
-        $('#chatline').val('');
-      }
-      return;
-    } else if (ev.key === 'Tab') {
-      chatTabComplete();
-      ev.preventDefault();
-      return false;
-    } else if (ev.key === 'ArrowUp') {
-      unsentMsg = $('#chatline').val();
-      if (CHATHISTIDX > 0) {
-        CHATHISTIDX--;
-        $('#chatline').val(CHATHIST[CHATHISTIDX]);
-      }
-      ev.preventDefault();
-      return false;
-    } else if (ev.key === 'ArrowDown') {
-      if (CHATHISTIDX === CHATHIST.length - 1 && unsentMsg !== null) {
-        CHATHISTIDX++;
-        $('#chatline').val(unsentMsg);
-        unsentMsg = null;
-      } else if (CHATHISTIDX < CHATHIST.length - 1) {
-        CHATHISTIDX++;
-        $('#chatline').val(CHATHIST[CHATHISTIDX]);
-      }
-      ev.preventDefault();
-      return false;
     }
   });
 
@@ -7347,11 +7279,7 @@ var cydj = (function (exports) {
 
   $('#chatline, #chatbtn').off();
 
-  /*
-
-  let /** @type {string} */    // unsentMsg = null;
-
-  /*
+  let /** @type {string} */ unsentMsg = null;
 
   $('#chatline').on('keydown', (ev) => {
     if (ev.key === 'Enter') {
@@ -7410,7 +7338,6 @@ var cydj = (function (exports) {
     }
   });
 
-  */
 
   // TODO: Check what this does, I don't know what it does.
   // #chatbtn is a button thats disabled? Maybe? If I remember correctly.
@@ -8095,7 +8022,6 @@ var cydj = (function (exports) {
 
   exports.addVideo = addVideo;
   exports.camera = camera;
-  exports.chatTabComplete = chatTabComplete;
   exports.getChatStats = getChatStats;
   exports.insertText = insertText;
   exports.prevVideo = prevVideo;
