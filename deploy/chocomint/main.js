7b4cb2458307e77ca8c324b479a951197fa27be6
ItMePeachy <106801886+ItMePeachy@users.noreply.github.com>
2023-01-08 12:03:45 +0000

    Deploying to deploy from @ papertek/CyDJ@2910e57e4b28221f0a82db3653c2f4d882548496 🚀

--- deploy/chocomint/main.js
+++ deploy/chocomint/main.js
@@ -4015,7 +4015,7 @@ var cydj = (function (exports) {
 
   const ChannelName_Caption = 'CyDJ';
 
-  const Version_Now$1 = 'CyDJPre1.3.23.0';
+  const Version_Now$1 = 'CyDJPre1.8.23.0';
 
   const HeaderDropMenu_Title = 'Information';
 
