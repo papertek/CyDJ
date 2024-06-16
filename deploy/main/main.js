534fe15902da5b63b39a99b6ca0e256c810b3beb
ItMePeachy <106801886+ItMePeachy@users.noreply.github.com>
2024-06-16 13:17:18 +0000

    Deploying to deploy from @ papertek/CyDJ@44361d9a0fc50afab342af176a06a77be324c3a1 🚀

--- deploy/main/main.js
+++ deploy/main/main.js
@@ -4024,19 +4024,19 @@ var cydj = (function (exports) {
 
   // TODO: Move all these into a file.
 
-  const Favicon_URL = 'https://cdn.7tv.app/emote/614e8c0b20eaf897465a4c9d/1x.webp';
+  const Favicon_URL = 'https://cdn.7tv.app/emote/60b8287355c320f0e8e92f64/1x.webp';
 
-  const MiniLogo_URL = 'https://cdn.7tv.app/emote/614e8c0b20eaf897465a4c9d/1x.webp';
+  const MiniLogo_URL = 'https://cdn.7tv.app/emote/60b8287355c320f0e8e92f64/1x.webp';
 
   const ChannelName_Caption = 'CyDJ';
 
-  const Version_Now = 'CyDJEdge2.9.24.0';
+  const Version_Now = 'CyDJEdge6.16.24.0';
 
   const HeaderDropMenu_Title = 'Information';
 
   const RulesBtn_Caption = 'Read Channel Rules';
 
-  const TitleIcon_URL = 'https://cdn.7tv.app/emote/6040a8bccf6746000db10348/2x.webp';
+  const TitleIcon_URL = 'https://cdn.7tv.app/emote/62740ee56fc5e131afe3d18e/1x.webp';
 
   const TitleBarDescription_Caption = 'Now Playing:';
 
@@ -4166,7 +4166,7 @@ var cydj = (function (exports) {
   ];
 
   const EmptyCornerBackground = [
-    'https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_a054f4001b6d4f098e7969c988debd18/default/light/2.0',
+    /* 'https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_a054f4001b6d4f098e7969c988debd18/default/light/2.0',
     'https://cdn.betterttv.net/emote/5dfc5d868608fb0da4120b59/2x',
     'https://raw.githubusercontent.com/papertek/CyDJ/beta/images/main/891194776798498826%20(1).gif',
     'https://raw.githubusercontent.com/papertek/CyDJ/beta/images/main/small.png',
@@ -4175,7 +4175,7 @@ var cydj = (function (exports) {
     'https://raw.githubusercontent.com/papertek/CyDJ/beta/images/main/mud.png',
     'https://raw.githubusercontent.com/papertek/CyDJ/beta/images/main/xso.png',
     'https://raw.githubusercontent.com/papertek/CyDJ/beta/images/main/pacific.gif',
-    'https://raw.githubusercontent.com/papertek/CyDJ/beta/images/main/pushing%20ass.png',
+    'https://raw.githubusercontent.com/papertek/CyDJ/beta/images/main/pushing%20ass.png', */
   ];
 
   // /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -4340,7 +4340,7 @@ var cydj = (function (exports) {
 
   const WEBKIT = 'webkitRequestAnimationFrame' in window;
   new Audio('https://github.com/papertek/CyDJ/raw/beta/misc/dropit.wav');
-  const HEY = new Audio('https://github.com/ItMePeachy/PeachyRoom/raw/beta/misc/yippee.mp3');
+  const HEY = new Audio('https://github.com/papertek/CyDJ/raw/master/misc/hey.wav');
   const NAY = new Audio('https://github.com/ItMePeachy/PeachyRoom/raw/beta/misc/scream.mp3');
   CHATSOUND.volume = 0.4;
 
